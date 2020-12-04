6e3ba6efa9abe728673529461cedce29bec66033
Harry Norrman <harry.norrman@gmail.com>
2020-12-04 15:11:35 +0100

    Solver algorithm changed
    Much simpler, still isn't working

--- src/sudoku/Solver.java
+++ src/sudoku/Solver.java
@@ -3,19 +3,21 @@ package sudoku;
 public class Solver implements SudokuSolver {
 	private int[][] grid;
 	private int[][] unsolvedGrid;
+
 	public Solver(int[][] grid) {
 		this.grid = grid;
 		this.unsolvedGrid = grid;
 	}
+
 	@Override
 	public void setNumber(int row, int col, int number) {
 		grid[row][col] = number;
-		
+
 	}
 
 	@Override
 	public boolean trySetNumber(int row, int col, int number) {
-        return isLegal(row,col,number);
+		return isLegal(row, col, number);
 	}
 
 	@Override
@@ -26,7 +28,7 @@ public class Solver implements SudokuSolver {
 	@Override
 	public void removeNumber(int row, int col) {
 		grid[row][col] = 0;
-		
+
 	}
 
 	@Override
@@ -36,83 +38,69 @@ public class Solver implements SudokuSolver {
 
 	@Override
 	public boolean solve() {
-        //for (int i = 0; i < 9; i++) {
-        //    for (int j = 0; j < 9; j++) {
-        if (grid[0][0] == 0)
-            	    return solve(0,0);
-         //   	}
-        //}
-        return true;
+		return solve(0, 0);
 	}
 
-    private boolean solve(int i, int j) {
-    	int currNumber = grid[i][j] +1;
-    	for (int number = currNumber; number <= 9; number++) {
-    		int nexti = i+1;
-    		int nextj = j+1;
-    		if(isLegal(i,j, number) && unsolvedGrid[i][j] == 0) {
-    			grid[i][j] = number;
-    			if(nexti > 8 && nextj > 8) {
-    				return true;
-    			}
-    			if(nexti > 8 && nextj < 8) {
-    				solve(0,nextj);
-    			}
-    			else if(nexti < 8){
-    				solve(nexti, j);
-    			}
-    			return false;
-    		}
-    	}
-    	if(grid[0][0] != 0) {
-    		return false;    		
-    	}
-    	else {
-    		if(i > 0) {
-    			solve(i-1,j);
-    		}
-    		else {
-    			solve(9,j-1);
-    		}
-    	}
-    	return false;
-    }
+	private boolean solve(int i, int j) {
+		// System.out.println("currNumber and place: " +currNumber + " " + i + " " + j);
+		int nexti = i + 1;
+		int nextj = j;
+		if (nexti > 8) {
+			nexti = 0;
+			nextj++;
+		}
+		if (getNumber(i, j) == 0) {
+			for (int number = 1; number <= 9; number++) {
+				if (isLegal(i, j, number)) {
+					System.out.println("number placed at: " + i + " " + j + " " + number);
+					setNumber(i, j, number);
+					if (solve(nexti, nextj)) {
+						return true;
+					} else {
+						setNumber(i, j, 0);
+					}
+				}
+			}
+			return false;
+		}
+		return true;
+	}
 
-    private boolean checkRow(int row, int number) {
-        for (int i = 0; i < 9; i++ ) {
-            if (grid[row][i] == number) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private boolean checkRow(int row, int number) {
+		for (int i = 0; i < 9; i++) {
+			if (grid[row][i] == number) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private boolean checkCol(int col, int number) {
-        for (int i = 0; i < 9; i++ ) {
-            if (grid[i][col] == number) {
-                return true;
-            }
-        }
-        return false;
-    }
-    private boolean checkRegion(int row, int col, int number) {
-        int r = row - row % 3;
-        int c = col - col % 3;
+	private boolean checkCol(int col, int number) {
+		for (int i = 0; i < 9; i++) {
+			if (grid[i][col] == number) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    	for (int i = r; i < r+3; i++ ) {
-    		for(int n =c ; n < c+3; n ++) {
-    			if (grid[i][n] == number)
-                    return true;
-            }
-        }
-        return false;
-    }
+	private boolean checkRegion(int row, int col, int number) {
+		int r = row - row % 3;
+		int c = col - col % 3;
 
-    private boolean isLegal(int row, int col, int number) {
-        return !(checkRow(row, number) || checkCol(col, number) || checkRegion(row, col, number));
-    }
+		for (int i = r; i < r + 3; i++) {
+			for (int n = c; n < c + 3; n++) {
+				if (grid[i][n] == number)
+					return true;
+			}
+		}
+		return false;
+	}
 
-    
+	private boolean isLegal(int row, int col, int number) {
+		return !(checkRow(row, number) || checkCol(col, number) || checkRegion(row, col, number));
+	}
+
 	@Override
 	public int[][] getNumbers() {
 		return grid;
